9eeab1b4d3fc78fca2b4ada2213a8492d9ae1c23
jfullerco <jonathan@jfuller.co>
2021-03-01 12:09:16 -0500

    1 file modified

--- src/Services/siteService.js
+++ src/Services/siteService.js
@@ -6,4 +6,9 @@ const getSite = async (id) => {
   return await http.get(`/rest/sites/${id}?metafields=true&fetchchildren=true`)
 
 }
+
+const postSite = async (id, data) => {
+  return await http.post(`/rest/clients/${id}/sites`, data)
+}
+
 export default getSite
