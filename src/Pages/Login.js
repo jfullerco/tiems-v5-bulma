5b6eed001fb389b7187fb72fe7ed2e83569d1d5d
jfullerco <jonathan@jfuller.co>
2021-03-02 14:15:49 -0500

    1 file modified

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -21,7 +21,6 @@ export default function Login() {
         login.tiemsUser == user && login.tiemsPass == pass ? 
           (
             userContext.setClientLoggedIn(true),
-            
             localStorage.setItem('clientID', login._parent_id),
             localStorage.setItem('LoggedIn', "true"),
             history.push("/")
@@ -39,53 +38,68 @@ export default function Login() {
     const {name, value} = event.target
     setLoginAttempt({...loginAttempt, [name]: value})
   }
+
   const handleSaveMe = event => {
     setSaveMe(!saveMe)
   }
 
   return(
-    <div className="column is-half">    
-          <div className="field">
+    <>
+      <div className="block"> 
+        <section className="hero is-info">
+          <div className="hero-body">
+            <p className="title">Login</p>
+          </div>
+        </section>
+      </div>
+
+      <div className="columns is-mobile is-centered">    
+      <div className="column is-half">
+      <div className="block">    
+        <div className="field">
           <div className="control">
-          <input
-            type="text"
-            placeholder="user"
-            name="user"
-            value={loginAttempt.user}
-            onChange={handleInputChange}
-            className="input is-rounded"
-          />
-         </div>
-          
+              <input
+                type="text"
+                placeholder="user"
+                name="user"
+                value={loginAttempt.user}
+                onChange={handleInputChange}
+                className="input is-rounded"
+              />
           </div>
-          <div className="field">
+        </div>
+        <div className="field">
           <div className="control">
-          <input
-            type="password"
-            placeholder="pass"
-            name="pass"
-            value={loginAttempt.pass}
-            onChange={handleInputChange}
-            className="input is-rounded"
-          />
+            <input
+              type="password"
+              placeholder="pass"
+              name="pass"
+              value={loginAttempt.pass}
+              onChange={handleInputChange}
+              className="input is-rounded"
+            />
           </div>
-          </div>
-          <div className="field is-grouped is-hidden">
+        </div>
+        <div className="field is-grouped is-hidden">
           <input
             type="checkbox"
             name="save"
             onChange={handleSaveMe}
             className="checkbox"
-          />  <label className="label is-small">Remember me </label>
-          </div>
-          <div className="control">
+          />  
+          <label className="label is-small">Remember me </label>
+        </div>
+        <div className="control">
           <button
-            className="button is-primary is-rounded is-fullwidth"
+            className="button is-info is-rounded is-fullwidth"
             type="submit"
             onClick={()=>handleSubmit(loginAttempt)}
           >Login</button>
-          </div>  
-    </div>
+        </div>
+      </div>
+      </div>  
+      </div>
+    </>
   )
 
 }
