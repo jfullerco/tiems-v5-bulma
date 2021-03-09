77e416c9d5d75f75cf3829181316342e203291c1
jfullerco <jonathan@jfuller.co>
2021-03-08 19:05:40 -0500

    1 file modified

--- src/stateReducer.js
+++ src/stateReducer.js
@@ -25,22 +25,11 @@ export default (state, action) => {
         ...state,
         sites: action.payload
       };
-    case "FOCUS_SITE":
-      const inFocusSiteID = action.payload
-      const inFocusSite = state.sites.map(site => {
-        site._id === inFocusSiteID._id ? {site} : "No Site Details"
-      })
-      return inFocustSite;
     case "GET_ASSETS":
       return {
         ...state,
         assets: action.payload
       };
-    case "FOCUS_ASSET":
-      const inFocusAssetID = action.payload
-      const inFocusAsset = state.assets.map(asset => {
-        asset._id === inFocusAssetID._id ? {asset} : "No Asset Details"
-      })
-      return inFocusAsset;
+    
   };
 }
